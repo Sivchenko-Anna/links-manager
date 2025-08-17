256823bfa7aa1a692afc0d2983a0580f109b1711
Sivchenko-Anna <asivchenko@animaccord.dev>
2025-08-17 18:07:57 +1000

    feat: implement AuthTabs component for login and registration

---
+++ src/components/AuthForm/AuthTabs.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+import Tabs from 'primevue/tabs';
+import TabList from 'primevue/tablist';
+import Tab from 'primevue/tab';
+import TabPanels from 'primevue/tabpanels';
+import TabPanel from 'primevue/tabpanel';
+
+const activeTab = ref('login'); // login | register | reset password
+</script>
+
+<template>
+    <Tabs v-model:value="activeTab">
+        <TabList>
+            <Tab value="login">Вход</Tab>
+            <Tab value="register">Регистрация</Tab>
+        </TabList>
+        <TabPanels>
+            <TabPanel value="login">
+                <p>Login Form Placeholder</p>
+            </TabPanel>
+            <TabPanel value="register">
+                <p>Register Form Placeholder</p>
+            </TabPanel>
+        </TabPanels>
+    </Tabs>
+</template>
--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,7 +1,11 @@
+<script setup>
+import AuthTabs from '@/components/AuthForm/AuthTabs.vue';
+</script>
+
 <template>
     <div class="flex justify-center items-center min-h-screen">
-        <div class="p-6 bg-white shadow-lg rounded-lg w-full md:w-110">
-            Авторизация/Регистрация
+        <div class="p-6 bg-white shadow-lg rounded-lg w-full md:w-110 dark:bg-black">
+            <AuthTabs />
         </div>
     </div>
 </template>
