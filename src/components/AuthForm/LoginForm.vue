7ef4e4a899a7adf07007f5099efa86eda6030f23
Sivchenko-Anna <asivchenko@animaccord.dev>
2025-08-20 15:45:45 +1000

    feat: enhance LoginForm with validation messages for email and password fields

--- src/components/AuthForm/LoginForm.vue
+++ src/components/AuthForm/LoginForm.vue
@@ -5,6 +5,7 @@ import { zodResolver } from '@primevue/forms/resolvers/zod';
 import { Form } from '@primevue/forms';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
+import Message from 'primevue/message';
 
 const formData = ref({
     email: '',
@@ -19,15 +20,21 @@ const rules = z.object({
 })
 
 const resolver = ref(zodResolver(rules));
+
+const handleSubmit = async ({valid}) => {
+    console.log('Form submitted:', valid);
+};
 </script>
 
 <template>
-    <Form v-slot="$form" :resolver="resolver" :initial-values="formData" :validate-on-blur :validate-on-value-update="false">
+    <Form v-slot="$form" :resolver="resolver" :initial-values="formData" :validate-on-blur="true" :validate-on-value-update="false" @submit="handleSubmit">
       <div class="mb-3">
         <InputText name="email" type="text" placeholder="Введите email" v-model="formData.email" class="w-full" />
+        <Message v-if="$form.email?.invalid" severity="error" variant="simple" size="small">{{ $form.email.error.message }}</Message>
       </div>
       <div class="mb-3">
         <InputText name="password" type="password" placeholder="Введите пароль" v-model="formData.password" class="w-full" />
+        <Message v-if="$form.password?.invalid" severity="error" variant="simple" size="small">{{ $form.password.error.message }}</Message>
       </div>
       <span class="cursor-pointer mb-3 block" @click="emit('reset-password')">Забыли пароль?</span>
       <div class="grid grid-cols-2 gap-3">
