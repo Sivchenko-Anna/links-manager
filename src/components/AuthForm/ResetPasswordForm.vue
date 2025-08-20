869049936db4d2eded42f9371d5f2f34760d0d59
Sivchenko-Anna <asivchenko@animaccord.dev>
2025-08-20 15:52:05 +1000

    feat: implement validation and error messages in ResetPasswordForm

--- src/components/AuthForm/ResetPasswordForm.vue
+++ src/components/AuthForm/ResetPasswordForm.vue
@@ -1,20 +1,31 @@
 <script setup>
 import { ref } from 'vue';
+import { z } from 'zod';
+import { zodResolver } from '@primevue/forms/resolvers/zod';
+import { Form } from '@primevue/forms';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
+import Message from 'primevue/message';
 
 const formData = ref({
     email: '',
 });
+
+const rules = z.object({
+  email: z.email({message: 'Введите корректный email'}),
+})
+
+const resolver = ref(zodResolver(rules));
 </script>
 
 <template>
-    <form>
+    <Form v-slot="$form" :resolver="resolver" :initial-values="formData" :validate-on-blur="true" :validate-on-value-update="false" @submit="handleSubmit">
       <div class="mb-3">
         <InputText name="email" type="text" placeholder="Введите email" v-model="formData.email" class="w-full" />
+        <Message v-if="$form.email?.invalid" severity="error" variant="simple" size="small">{{ $form.email.error.message }}</Message>
       </div>
       <div class="grid">
         <Button type="submit" label="Сброс пароля" class="w-full" />
       </div>
-    </form>
+    </Form>
 </template>
