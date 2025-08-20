30b7dd1d1fd9abc14db7a5a7e7e4dbed50033649
Sivchenko-Anna <asivchenko@animaccord.dev>
2025-08-20 13:31:26 +1000

    feat: add LoginForm  in AuthTabs

--- src/components/AuthForm/AuthTabs.vue
+++ src/components/AuthForm/AuthTabs.vue
@@ -5,6 +5,7 @@ import TabList from 'primevue/tablist';
 import Tab from 'primevue/tab';
 import TabPanels from 'primevue/tabpanels';
 import TabPanel from 'primevue/tabpanel';
+import LoginForm from '@/components/AuthForm/LoginForm.vue'
 
 const activeTab = ref('login'); // login | register | reset password
 </script>
@@ -17,7 +18,7 @@ const activeTab = ref('login'); // login | register | reset password
         </TabList>
         <TabPanels>
             <TabPanel value="login">
-                <p>Login Form Placeholder</p>
+                <LoginForm />
             </TabPanel>
             <TabPanel value="register">
                 <p>Register Form Placeholder</p>
---
+++ src/components/AuthForm/LoginForm.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+
+const formData = ref({
+    email: '',
+    password: ''
+});
+</script>
+
+<template>
+    <form>
+      <div class="mb-3">
+        <InputText name="email" type="text" placeholder="Введите email" v-model="formData.email" class="w-full" />
+      </div>
+    </form>
+</template>
