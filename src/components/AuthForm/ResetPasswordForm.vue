f8c228cf32fbe691a470c7e98a549aaeac113c0d
Sivchenko-Anna <asivchenko@animaccord.dev>
2025-08-20 15:03:00 +1000

    feat: create ResetPasswordForm component

---
+++ src/components/AuthForm/ResetPasswordForm.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+
+const formData = ref({
+    email: '',
+});
+</script>
+
+<template>
+    <form>
+      <div class="mb-3">
+        <InputText name="email" type="text" placeholder="Введите email" v-model="formData.email" class="w-full" />
+      </div>
+      <div class="grid">
+        <Button type="submit" label="Сброс пароля" class="w-full" />
+      </div>
+    </form>
+</template>
