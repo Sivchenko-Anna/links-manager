3be159df9e5a744e16957cc09eefe158174cedce
Sivchenko-Anna <asivchenko@animaccord.dev>
2025-09-10 14:37:45 +1000

    feat: add ResetPassword route and create ResetPassword component with NewPasswordForm

---
+++ src/views/ResetPassword.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NewPasswordForm from '@/components/AuthForm/NewPasswordForm.vue'
+</script>
+
+<template>
+  <div class="flex justify-center items-center min-h-screen">
+    <div class="p-6 bg-white dark:bg-zinc-900 shadow-lg rounded-lg w-full md:w-110 border border-gray-200 dark:border-gray-700">
+      <NewPasswordForm />
+    </div>
+  </div>
+</template>
