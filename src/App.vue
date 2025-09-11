c6ff3acceaaa9da327cf7c604466015962411089
Sivchenko-Anna <asivchenko@animaccord.dev>
2025-09-11 13:42:02 +1000

    feat: initialize userStore and fetch user data on app mount

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script setup>
+import { onMounted } from 'vue';
 import { RouterView } from 'vue-router'
+import { useUserStore } from './stores/userStore.js';
 import AppHeader from './components/AppHeader.vue';
+
+const userStore = useUserStore();
+onMounted(async() => {
+  await userStore.getUser()
+})
 </script>
 
 <template>
