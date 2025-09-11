4b5eedbb67b183e0fe1196fbf4a9298447e0d698
Sivchenko-Anna <asivchenko@animaccord.dev>
2025-09-11 13:17:13 +1000

    feat: add AppHeader component to the main application layout

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import Button from 'primevue/button'
+    import Avatar from 'primevue/avatar'
+    import Menubar from 'primevue/menubar'
+</script>
+
+<template>
+  <div class="mb-5">
+    <Menubar>
+      <template #start>
+        <div class="flex items-center gap-2">
+          <span class="font-bold">Link Manager</span>
+          <div class="flex items-center gap-2">
+            <Button icon="pi pi-link" rounded />
+            <Button icon="pi pi-folder" rounded />
+          </div>
+        </div>
+      </template>
+      <template #end>
+        <div class="flex items-center gap-2">
+          <Avatar label="A" size="large" shape="circle" />
+          <Button icon="pi pi-sign-out" rounded severity="secondary" />
+        </div>
+      </template>
+    </Menubar>
+  </div>
+</template>
