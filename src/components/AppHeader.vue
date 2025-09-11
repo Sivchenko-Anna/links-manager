513e7e4f0fd09f8871774aba726f532c847d00d9
Sivchenko-Anna <asivchenko@animaccord.dev>
2025-09-11 15:37:33 +1000

    feat: implement sign-out functionality and user state management

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,31 @@
 <script setup>
   import { computed } from 'vue';
+  import { useRouter } from 'vue-router'
   import { useUserStore } from '@/stores/userStore.js';
+  import { useAuth } from '@/composables/useAuth'
+  import { useToastNofitications } from '@/composables/useToastNofitications.js'
   import Button from 'primevue/button'
   import Avatar from 'primevue/avatar'
   import Menubar from 'primevue/menubar'
 
-
   const userStore = useUserStore();
+  const { signOut, errorMessage } = useAuth()
+  const { showToast } = useToastNofitications()
+  const router = useRouter();
 
   const emailFirstLetter = computed(() => {
     return userStore.user?.email ? userStore.user?.email[0].toUpperCase() : ''
   })
+
+  const singOutUser = async () => {
+    try {
+      await signOut()
+      userStore.clearUser()
+      await router.replace({ name: 'auth' })
+    } catch {
+      showToast('error', 'Ошибка выхода', errorMessage.value)
+    }
+  }
 </script>
 
 <template>
@@ -28,7 +43,7 @@
       <template #end>
         <div class="flex items-center gap-2">
           <Avatar :label="emailFirstLetter" size="large" shape="circle" />
-          <Button icon="pi pi-sign-out" rounded severity="secondary" />
+          <Button icon="pi pi-sign-out" rounded severity="secondary" @click="singOutUser" />
         </div>
       </template>
     </Menubar>
