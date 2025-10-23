21648de65828abfc666f1a9543aa4cfc67307c27
Sivchenko-Anna <asivchenko@animaccord.dev>
2025-10-23 11:27:10 +1000

    feat: add CategoriesModal component and integrate into AppHeader with toggle functionality

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,34 +1,38 @@
 <script setup>
-  import { computed } from 'vue';
-  import { useRouter } from 'vue-router'
-  import { useUserStore } from '@/stores/userStore.js';
-  import { useAuth } from '@/composables/useAuth'
-  import { useToastNofitications } from '@/composables/useToastNofitications.js'
-  import Button from 'primevue/button'
-  import Avatar from 'primevue/avatar'
-  import Menubar from 'primevue/menubar'
+import { computed, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useUserStore } from '@/stores/userStore.js'
+import { useAuth } from '@/composables/useAuth'
+import { useToastNofitications } from '@/composables/useToastNofitications.js'
+import Button from 'primevue/button'
+import Avatar from 'primevue/avatar'
+import Menubar from 'primevue/menubar'
+import CategoriesModal from '@/components/Modals/CategoriesModal.vue'
 
-  const userStore = useUserStore();
-  const { signOut, errorMessage } = useAuth()
-  const { showToast } = useToastNofitications()
-  const router = useRouter();
+const userStore = useUserStore()
+const { signOut, errorMessage } = useAuth()
+const { showToast } = useToastNofitications()
+const router = useRouter()
 
-  const emailFirstLetter = computed(() => {
-    return userStore.user?.email ? userStore.user?.email[0].toUpperCase() : ''
-  })
+const emailFirstLetter = computed(() => {
+  return userStore.user?.email ? userStore.user?.email[0].toUpperCase() : ''
+})
 
-  const singOutUser = async () => {
-    try {
-      await signOut()
-      userStore.clearUser()
-      await router.replace({ name: 'auth' })
-    } catch {
-      showToast('error', 'Ошибка выхода', errorMessage.value)
-    }
+const singOutUser = async () => {
+  try {
+    await signOut()
+    userStore.clearUser()
+    await router.replace({ name: 'auth' })
+  } catch {
+    showToast('error', 'Ошибка выхода', errorMessage.value)
   }
+}
+
+const CategoriesDialogVisible = ref(false)
 </script>
 
 <template>
+  <CategoriesModal v-model="CategoriesDialogVisible" />
   <div class="mb-5">
     <Menubar>
       <template #start>
@@ -36,7 +40,7 @@
           <span class="font-bold">Link Manager</span>
           <div class="flex items-center gap-2">
             <Button icon="pi pi-link" rounded />
-            <Button icon="pi pi-folder" rounded />
+            <Button icon="pi pi-folder" rounded @click="CategoriesDialogVisible = true" />
           </div>
         </div>
       </template>
---
+++ src/components/Modals/CategoriesModal.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Dialog from 'primevue/dialog'
+
+const modelValue = defineModel()
+</script>
+
+<template>
+  <Dialog header="Категории" v-model:visible="modelValue">
+    <p>Modal</p>
+  </Dialog>
+</template>
+
+<style scoped></style>
