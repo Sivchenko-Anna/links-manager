149dc80ad67f7ea9fd9ec158830b8b7e5be9a4d9
Sivchenko-Anna <asivchenko@animaccord.dev>
2025-10-27 14:06:52 +1000

    feat: add AppLoader component with primevue ProgressSpinner for centralized loading visuals

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,14 @@
+<script setup>
+import ProgressSpinner from 'primevue/progressspinner'
+</script>
+
+<template>
+  <div class="flex justify-center">
+    <ProgressSpinner
+      class="w-[50px] h-[50px]"
+      strokeWidth="8"
+      fill="transparent"
+      animationDuration=".5s"
+    />
+  </div>
+</template>
