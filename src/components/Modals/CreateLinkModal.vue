8be192b07ba252c80a97ba239639b43ad5961296
Sivchenko-Anna <asivchenko@animaccord.dev>
2025-10-27 21:37:39 +1000

    feat: display a message when no categories exist in CreateLinkModal to guide users

--- src/components/Modals/CreateLinkModal.vue
+++ src/components/Modals/CreateLinkModal.vue
@@ -193,52 +193,57 @@ watch(modelValue, async (newValue) => {
     >
       <AppLoader v-if="isLoading" />
       <template v-else>
-        <div class="mb-3">
-          <InputText
-            name="name"
-            v-model="formInputs.name"
-            class="w-full"
-            autocomplete="off"
-            placeholder="Название ссылки"
-          />
-          <Message v-if="$form.name?.invalid" severity="error" variant="simple" size="small">
-            {{ $form.name.error.message }}
-          </Message>
+        <div class="font-bold text-center" v-if="!categoriesList.length">
+          Сначала добавьте хотя бы одну категорию
         </div>
-        <div class="mb-3">
-          <InputText
-            name="url"
-            v-model="formInputs.url"
-            class="w-full"
-            autocomplete="off"
-            placeholder="Ссылка"
-          />
-          <Message v-if="$form.url?.invalid" severity="error" variant="simple" size="small">
-            {{ $form.url.error.message }}
-          </Message>
-        </div>
-        <div class="mb-3">
-          <Select
-            v-model="formInputs.category"
-            :options="categoriesList"
-            optionLabel="name"
-            placeholder="Выберите категорию"
-            class="w-full"
-          />
-        </div>
-        <div class="mb-3">
-          <Textarea
-            v-model="formInputs.description"
-            class="w-full resize-none"
-            placeholder="Описание"
-          />
-        </div>
-        <div class="mb-3 flex items-center gap-2">
-          <Checkbox v-model="formInputs.isFavorite" inputId="isFavorite" binary />
-          <label for="isFavorite">Добавить в избранное</label>
-        </div>
-        <div class="flex justify-end gap-2 mt-4">
-          <Button :label="textButton" :loading="isLoadingButton" type="submit" />
+        <div v-else>
+          <div class="mb-3">
+            <InputText
+              name="name"
+              v-model="formInputs.name"
+              class="w-full"
+              autocomplete="off"
+              placeholder="Название ссылки"
+            />
+            <Message v-if="$form.name?.invalid" severity="error" variant="simple" size="small">
+              {{ $form.name.error.message }}
+            </Message>
+          </div>
+          <div class="mb-3">
+            <InputText
+              name="url"
+              v-model="formInputs.url"
+              class="w-full"
+              autocomplete="off"
+              placeholder="Ссылка"
+            />
+            <Message v-if="$form.url?.invalid" severity="error" variant="simple" size="small">
+              {{ $form.url.error.message }}
+            </Message>
+          </div>
+          <div class="mb-3">
+            <Select
+              v-model="formInputs.category"
+              :options="categoriesList"
+              optionLabel="name"
+              placeholder="Выберите категорию"
+              class="w-full"
+            />
+          </div>
+          <div class="mb-3">
+            <Textarea
+              v-model="formInputs.description"
+              class="w-full resize-none"
+              placeholder="Описание"
+            />
+          </div>
+          <div class="mb-3 flex items-center gap-2">
+            <Checkbox v-model="formInputs.isFavorite" inputId="isFavorite" binary />
+            <label for="isFavorite">Добавить в избранное</label>
+          </div>
+          <div class="flex justify-end gap-2 mt-4">
+            <Button :label="textButton" :loading="isLoadingButton" type="submit" />
+          </div>
         </div>
       </template>
     </Form>
