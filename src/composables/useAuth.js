038cb58311d760abe428cbe08f6ff68dfbe18b05
Sivchenko-Anna <asivchenko@animaccord.dev>
2025-10-27 21:07:32 +1000

    chore: update resetPassword redirect URL to production environment

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -29,7 +29,7 @@ export function useAuth() {
   const resetPassword = async ( email ) => {
     return await handleRequest(async () => {
       const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
-        redirectTo: 'http://localhost:5173/reset-password',
+        redirectTo: 'https://links-manager-app.netlify.app/reset-password',
       })
       if (error) throw error
       return data
